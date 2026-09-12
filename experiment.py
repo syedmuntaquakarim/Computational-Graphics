73e03c201f0a45ddfa9be69f636b722b6b205f68
Syed Muntaqua Karim <syedmuntaquakarim@gmail.com>
2026-09-12 20:53:47 +0530

    doing some experiments

---
+++ experiment.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 1. Parameters
+a = 1 
+k = 7/4
+
+# 2. Polar Domain (1000 points from 0 to 2*pi)
+theta = np.linspace(0, 8 * np.pi, 1000)
+
+# 3. Core Equation
+r = a * np.cos(7/4 * theta) + a * np.sin(3 * theta)
+
+# 4. Convert to Cartesian Coordinates
+x = r * np.cos(theta)
+y = r * np.sin(theta)
+
+# 5. Plot
+plt.plot(x, y, color='cyan')
+plt.axis('equal')
+plt.axis('off')
+plt.show()
+
